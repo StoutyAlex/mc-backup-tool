647077fdda3dc4b4d88b1410d805db097684b0fe
Alex Stout <alexstout009@hotmail.com>
2019-08-25 13:08:23 +0100

    added discard old builds

--- run.js
+++ run.js
@@ -2,7 +2,6 @@
 require('dotenv').config();
 const fs = require('fs');
 const tar = require('tar');
-const path = require('path');
 const moment = require('moment');
 const { argv } = require('yargs')
   .option('world', {
@@ -12,7 +11,8 @@ const { argv } = require('yargs')
   .option('nether', {
     alias: 'n',
     default: 'world_nether'
-  });
+  })
+  .boolean('upload');
 
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({
@@ -20,27 +20,44 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_KEY_ID
 });
 
+const date = moment();
+
 AWS.config.logger = console;
 
 const zipBackup = (worlds, backupName) => new Promise((resolve, reject) => {
-  console.log(`Zipping ${worlds.length} world(s) -> `, backupName);
+  console.log(`Zipping ${worlds.length} world(s) ->`, backupName);
 
-  const currentTime = moment();
-
-  fs.writeFileSync('backup-info.txt', JSON.stringify({
-    backup_date: currentTime.format('D-M-YYYY'),
-    backup_time: currentTime.format('H:m:s'),
+  fs.writeFileSync('./server-backups/backup-info.txt', JSON.stringify({
+    backup_date: date.format('D-M-YYYY'),
+    backup_time: date.format('H:m:s'),
   }, null, 4));
 
   tar.c({
     gzip: true,
-    file: backupName
-  }, [...worlds, "backup-info.txt"])
+    file: `./server-backups/${backupName}`,
+  }, [...worlds, "./server-backups/backup-info.txt"])
     .then(() => {
-      fs.unlinkSync('backup-info.txt');
+      fs.unlinkSync('./server-backups/backup-info.txt');
       resolve();
     })
     .catch(reject)
+});
+
+const uploadToS3 = (backupName) => new Promise((resolve, reject) => {
+  console.log('Uploading to s3...');
+  const params = {
+    Bucket: process.env.BACKUP_TOOL_S3_BUCKET,
+    Key: backupName,
+    Body: fs.createReadStream(`./server-backups/${backupName}`)
+  };
+
+  s3.upload(params, (error, data) => {
+    if (error) {
+      return reject(error);
+    }
+    console.log('Uploaded, download from:', data.Location);
+    return resolve(data);
+  });
 });
 
 module.exports = (async () => {
@@ -56,16 +73,31 @@ module.exports = (async () => {
     worldsToBackup.push(argv.nether);
   }
 
-  const backupName = `server-backup-${moment().format('D-M-YYYY')}.tgz`;
+  if (!fs.existsSync('./server-backups')) {
+    fs.mkdirSync('server-backups');
+  }
+
+  const todaysDate = date.format('D-M-YYYY');
+  const currentTime = !argv.u ? date.format('-H:m:s') : '';
+
+  const backupName = `server-backup-${todaysDate}${currentTime}.tgz`;
+
   await zipBackup(worldsToBackup, backupName);
 
-  console.log(process.env);
+  if (argv.u)
+    await uploadToS3(backupName)
 
-  const params = {
-    Bucket: process.env.BACKUP_TOOL_S3_BUCKET,
-    Key: backupName,
-    Body: fs.createReadStream(backupName)
-  };
+  console.log('Backup completed');
 
-  s3.upload(params, (res) => console.log(res));
+  const oldBuildDate = date.subtract('7', 'days').format('D-M-YYYY');
+  const backupDirectory = fs.readdirSync('./server-backups');
+  console.log('Searching for old backups from', oldBuildDate);
+
+  backupDirectory.forEach(backup => {
+    if (backup.includes(oldBuildDate)) {
+      console.log('Removed', backup);
+      fs.unlinkSync(`./server-backups/${backup}`);
+    }
+  });
+
 })();
